5d3d7f41b37322250b06f529b88960372c0f195a
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-03-30 02:06:57 +0530

    feat: add dotenv for custom port configuration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({message: 'Hello MERN!'});
+})
 
 app.listen(PORT, (err) => {
     console.log(`Server started on port: ${PORT}`);
