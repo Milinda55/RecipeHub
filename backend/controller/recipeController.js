38e77925e3e550c28e432f991b98cceab61c9d7b
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-03-30 02:43:56 +0530

    Add test APIs and set req/res messages for each one

--- backend/controller/recipeController.js
+++ backend/controller/recipeController.js
@@ -3,4 +3,24 @@ const getRecipes = (req, res) => {
     )
 }
 
-module.exports = {getRecipes}
+const getRecipe = (req, res) => {
+    res.json({message: 'Hello Recipe!'}
+    )
+}
+
+const addRecipes = (req, res) => {
+    res.json({message: 'Add Recipes!'}
+    )
+}
+
+const editRecipe = (req, res) => {
+    res.json({message: 'Edit Recipe!'}
+    )
+}
+
+const deleteRecipe = (req, res) => {
+    res.json({message: 'Delete Recipe!'}
+    )
+}
+
+module.exports = {getRecipes, getRecipe, addRecipes, editRecipe, deleteRecipe}
--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,7 +1,12 @@
 const express = require('express');
-const {getRecipes} = require("../controller/recipeController");
+const {getRecipes, getRecipe, addRecipes, editRecipe, deleteRecipe} = require("../controller/recipeController");
 const router = express.Router();
 
 router.get('/', getRecipes);
+router.get('/:id', getRecipe);
+router.post('/', addRecipes);
+router.put('/:id', editRecipe);
+router.delete('/:id', deleteRecipe);
+
 
 module.exports = router;
