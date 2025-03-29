b5ca8e0065c5748d1fbb5051c59eeab82dd53c0b
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-03-30 04:14:10 +0530

    Connect the server with database

--- backend/config/connectionDb.js
+++ backend/config/connectionDb.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDb = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connected'));
 }
+
+module.exports = connectDb;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const connectDb = require('./config/connectionDb');
 
 const PORT = process.env.PORT || 3000;
+connectDb();
 
 app.use("/recipe", require('./routes/recipe'));
 
