6fc6f847a59c1626a001ec896ad0c079266867e3
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-04-29 19:05:27 +0530

    Implement the logic behind deleting the recipes

--- frontend/food-blog-app/src/components/RecipeItems.jsx
+++ frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, useLoaderData} from "react-router-dom";
 import foodImg from '../assets/Food-recipe.png';
 import { BsStopwatchFill } from "react-icons/bs";
 import { FaHeart } from "react-icons/fa6";
 import { FaEdit } from "react-icons/fa";
 import { MdDeleteOutline } from "react-icons/md";
+import axios from "axios";
 
 function RecipeItems(props) {
 
-    const allRecipes=useLoaderData();
+    const recipes=useLoaderData();
+    const [allRecipes, setAllRecipes] = useState()
     let path = window.location.pathname === "/myRecipe" ? true : false;
     console.log(allRecipes);
+
+    useEffect(()=> {
+        setAllRecipes(recipes)
+    },[recipes])
+
+    const onDelete=async(id)=> {
+        await axios.delete(`http://localhost:5000/recipe/${id}`)
+            .then((res)=>console.log(res))
+        setAllRecipes(recipes=>recipes.filter(recipe=>recipe._id !== id))
+    }
     return (
         <>
             <div className='card-container'>
@@ -29,7 +41,7 @@ function RecipeItems(props) {
                                         {(!path) ? < FaHeart /> :
                                             <div className='action'>
                                                 <Link to={`/editRecipe/${item._id}`} className="editIcon">< FaEdit /></Link>
-                                                < MdDeleteOutline className='deleteIcon' />
+                                                < MdDeleteOutline onClick={()=>onDelete(item._id)} className='deleteIcon' />
                                             </div>
                                         }
 
