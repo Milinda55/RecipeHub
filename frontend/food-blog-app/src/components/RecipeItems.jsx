9d558626dc48e94b908a2313d99eede72dd4417b
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-05-02 01:39:03 +0530

    Improve the user interface on recipe items

--- frontend/food-blog-app/src/components/RecipeItems.jsx
+++ frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -3,7 +3,7 @@ import {Link, useLoaderData} from "react-router-dom";
 import foodImg from '../assets/Food-recipe.png';
 import { BsStopwatchFill } from "react-icons/bs";
 import { FaHeart } from "react-icons/fa6";
-import { FaEdit } from "react-icons/fa";
+import {FaEdit, FaUtensils} from "react-icons/fa";
 import { MdDeleteOutline } from "react-icons/md";
 import axios from "axios";
 import {AuthContext} from "./AuthContext.jsx";
@@ -54,55 +54,67 @@ function RecipeItems(props) {
 
 
     return (
-        <>
-            <div className='card-container'>
-                {filteredRecipes?.map((item, index) => {
-                    const isFavourite = favItems.some(res => res._id === item._id);
+        <div className='recipe-grid'>
+            {filteredRecipes?.map((item, index) => {
+                const isFavourite = favItems.some(res => res._id === item._id);
+                const difficulty = ['Easy', 'Medium', 'Hard'][Math.floor(Math.random() * 3)];
 
-                    return (
-                        <div key={index} className='card'>
+                return (
+                    <div key={index} className='recipe-card'>
+                        <div className='card-image-container'>
                             <img
                                 src={`http://localhost:5000/images/${item.coverImage}`}
-                                width="120px"
-                                height="100px"
                                 alt={item.title}
+                                className='card-image'
                             />
-                            <div className='card-body'>
-                                <div className='title'>{item.title}</div>
-                                <div className='icons'>
-                                    <div className='timer'>
-                                        <BsStopwatchFill />
-                                        {item.time}
+                            <div className='card-badge'>{difficulty}</div>
+                            <button
+                                className={`favorite-btn ${isFavourite ? 'active' : ''}`}
+                                onClick={() => isLoggedIn ? favRecipe(item) : setIsOpen(true)}
+                                aria-label={isFavourite ? 'Remove from favorites' : 'Add to favorites'}
+                            >
+                                <FaHeart />
+                            </button>
+                        </div>
+                        <div className='card-content'>
+                            <h3 className='card-title'>{item.title}</h3>
+                            <div className='card-meta'>
+                            <span className='meta-item'>
+                                <BsStopwatchFill className='meta-icon' />
+                                {item.time} mins
+                            </span>
+                                <span className='meta-item'>
+                                <FaUtensils className='meta-icon' />
+                                    {item.category || 'Main Course'}
+                            </span>
+                            </div>
+                            <p className='card-description'>
+                                {item.description || 'A delicious recipe you must try!'}
+                            </p>
+                            <div className='card-actions'>
+                                <Link to={`/recipe/${item._id}`} className='view-btn'>
+                                    View Recipe
+                                </Link>
+                                {path && (
+                                    <div className='admin-actions'>
+                                        <Link to={`/editRecipe/${item._id}`} className='edit-btn'>
+                                            <FaEdit />
+                                        </Link>
+                                        <button
+                                            onClick={() => onDelete(item._id)}
+                                            className='delete-btn'
+                                            aria-label='Delete recipe'
+                                        >
+                                            <MdDeleteOutline />
+                                        </button>
                                     </div>
-                                    {!path ? (
-                                        <FaHeart
-                                            onClick={() => favRecipe(item)}
-                                            style={{
-                                                color: isFavourite ? "red" : "",
-                                                cursor: isLoggedIn ? "pointer" : "not-allowed",
-                                                opacity: isLoggedIn ? 1 : 0.6
-                                            }}
-                                            title={isLoggedIn ? "" : "Login to save favorites"}
-                                        />
-                                    ) : (
-                                        <div className='action'>
-                                            <Link to={`/editRecipe/${item._id}`} className="editIcon">
-                                                <FaEdit />
-                                            </Link>
-                                            <MdDeleteOutline
-                                                onClick={() => onDelete(item._id)}
-                                                className='deleteIcon'
-                                            />
-                                        </div>
-                                    )}
-                                </div>
+                                )}
                             </div>
                         </div>
-                    );
-                })}
-            </div>
-
-        </>
+                    </div>
+                );
+            })}
+        </div>
     );
 }
 
