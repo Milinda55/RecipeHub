2d130040af6575b2eac10a536802fd9aa498a8be
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-05-20 03:17:02 +0530

    Update the connectionDb file

--- backend/config/connectionDb.js
+++ backend/config/connectionDb.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const connectDb = async () => {
-    await mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connected'));
+    // await mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connected'));
+    try {
+        await mongoose.connect(process.env.MONGO_URI).then(()=> console.log('MongoDB Connected'));
+    } catch (error) {
+        console.error("MongoDB connection failed", error.message);
+        process.exit(1);
+    }
 }
 
 module.exports = connectDb;
